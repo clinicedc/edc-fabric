b3e9884c920ede3b41212611a8e28f18eb68771a
erikvw <ew2789@gmail.com>
2017-04-08 15:18:20 +0200

    fabfile

--- bcpp_fabric/new/fabfile/files/dmg.py
+++ bcpp_fabric/new/fabfile/files/dmg.py
@@ -1,4 +1,4 @@
-from fabric.api import cd, task, env, run, settings
+from fabric.api import cd, env, run
 
 from fabric.colors import red
 
@@ -6,22 +6,18 @@ from fabric.colors import red
 def dismount_dmg(volume_name=None):
     """Dismounts a dmg file on the remote host.
     """
-    with cd(volume_name):
-        with settings(warn_only=True):
-            result = run('diskutil unmount {}'.format(volume_name))
-            if result.failed:
-                print(red('{host} Dismount failed for {volume_name}'.format(
-                    host=env.host, volume_name=volume_name)))
+    result = run('diskutil unmount {}'.format(volume_name))
+    if result.failed:
+        print(red('{host} Dismount failed for {volume_name}'.format(
+            host=env.host, volume_name=volume_name)))
 
 
 def mount_dmg(dmg_path=None, dmg_filename=None, dmg_passphrase=None):
     """Mounts a dmg file on the remote host.
     """
-    env.prompts.update({'Enter disk image passphrase': dmg_passphrase})
+    env.prompts.update({'Enter disk image passphrase:': dmg_passphrase})
     dmg_path = dmg_path or env.dmg_path
     dmg_filename = dmg_filename or env.dmg_filename
+    run('diskutil unmount {}'.format(env.key_volume), warn_only=True)
     with cd(dmg_path):
-        result = run('hdiutil attach -stdinpass {}'.format(dmg_filename))
-        if result.failed:
-            print(red('{host} Mount failed for {dmg_filename}'.format(
-                host=env.host, dmg_filename=dmg_filename)))
+        run('hdiutil attach -stdinpass {}'.format(dmg_filename))
